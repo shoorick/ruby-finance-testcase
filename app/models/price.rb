c0fdc19f2dbd588ddd0fc1f4321c87d2df515b23
Alexander Sapozhnikov <shoorick@mail.ru>
2016-01-24 02:29:08 +0500

    Unnecessary braces removed

--- app/models/price.rb
+++ app/models/price.rb
@@ -2,7 +2,7 @@ class Price < ActiveRecord::Base
 
   attr_accessible :stock, :stock_id, :price, :datetime
 
-  validates :price, { numericality: { greater_than: 0 } }
+  validates :price, numericality: { greater_than: 0 }
 
   belongs_to :stock
 
