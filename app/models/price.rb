a83e3eef0bd1b9222455df0e74c17a740142086a
Alexander Sapozhnikov <shoorick@mail.ru>
2016-01-24 02:46:47 +0500

    Line attr_accessible with removed because attr_accessible removed from Rails 4+ — http://stackoverflow.com/questions/23437830/undefined-method-attr-accessible

--- app/models/price.rb
+++ app/models/price.rb
@@ -1,7 +1,5 @@
 class Price < ActiveRecord::Base
 
-  attr_accessible :stock, :stock_id, :price, :datetime
-
   validates :price, numericality: { greater_than: 0, message: 'Price should be positive' }
 
   belongs_to :stock
