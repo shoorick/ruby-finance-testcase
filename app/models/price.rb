e0c40bb7638beb9e3b61fa64235bf5bcc2ed5194
Alexander Sapozhnikov <shoorick@mail.ru>
2016-01-24 02:42:33 +0500

    Validation message fixed

--- app/models/price.rb
+++ app/models/price.rb
@@ -2,7 +2,7 @@ class Price < ActiveRecord::Base
 
   attr_accessible :stock, :stock_id, :price, :datetime
 
-  validates :price, numericality: { greater_than: 0 }, message: 'Price should be positive'
+  validates :price, numericality: { greater_than: 0, message: 'Price should be positive' }
 
   belongs_to :stock
 
